830fbdfcd6ba5878f1f2a7dbab74c2da46cd41d3
Avinash Chavan <avinashcahavn1443@gmail.com>
2022-06-11 19:42:01 +0530

    graphql express

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,70 @@
 import "reflect-metadata";
 import { createConnection, getRepository } from "typeorm";
 import { User } from "./entity/User";
-import { ApolloServer, AuthenticationError, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+
 const auth = require("./util/auth");
 const jwt = require("jsonwebtoken");
 
 // import resolvers = require("./graphql/resolver");
 // const typeDefs = require("./graphql/schema");
 const schema = require("./graphql/schema");
-const server = new ApolloServer({
-  schema,
-  context: auth,
-});
+// const server = new ApolloServer({
+//   schema,
+//   context: auth,
+//   csrfPrevention: true,
+//   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+// });
 
-createConnection()
-  .then(() => {
-    server.listen().then(({ url }) => {
-      console.log(`🚀  Server ready at ${url}`);
-    });
-  })
-  .catch((err) => console.log(err));
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    schema,
+    context: auth,
+    csrfPrevention: true,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  createConnection()
+    .then(async () => {
+      // server.listen().then(({ url }) => {
+      //   console.log(`🚀  Server ready at ${url}`);
+      // });
+      await server.start();
+      server.applyMiddleware({ app });
+      await new Promise<void>((resolve) =>
+        httpServer.listen({ port: 4000 }, resolve)
+      );
+      console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+      );
+    })
+    .catch((err) => console.log(err));
+}
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   csrfPrevention: true,
+//   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+// });
+
+// createConnection()
+//   .then(async() => {
+//     // server.listen().then(({ url }) => {
+//     //   console.log(`🚀  Server ready at ${url}`);
+//     // });
+//     await server.start();
+//     server.applyMiddleware({ app });
+//     await new Promise<void>((resolve) =>
+//       httpServer.listen({ port: 4000 }, resolve)
+//     );
+//     console.log(
+//       `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+//     );
+//   })
+//   .catch((err) => console.log(err));
+startApolloServer();
